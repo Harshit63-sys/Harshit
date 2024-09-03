7e527a41e467fa4f8416e771b8e32719c3d81485
Harshit63-sys <harshitgupta9526@gmail.com>
2024-09-03 11:13:34 +0530

    Add files via upload

--- app1.py
+++ app1.py
@@ -1,22 +1,22 @@
 import streamlit as st
-from pygments.styles.dracula import background
-
 st.title('My first app')
 st.write('Hello all')
-x=st.text_input('Which technology you want to learn')
+x=st.text_input('which technology you want to learn')
 if x=='Ai':
-    st.write('Kindly enrolled in python first')
-y=st.radio('are you graduate',option=['yes','No'])
-if y=='yes':
-    st.write('you can enroll in our diploma course')
+    st.write('kindly enroll in python first')
 else:
-    st.write('You can do Internship')
-
-z=st.selectbox('select the technology',('Python','Java','C++'))
+    st.write('sorry')
+y=st.radio('Are you Graduate',options=['Yes','No'])
+if y=='Yes':
+    st.write('You can enroll in our diploma course')
+else:
+    st.write('You are not eligible this diploma course')
+#a=st.button('show')
+#if a==1
+z=st.selectbox('Select the technology',('python','java','c++'))
 if z=='python':
-    st.write('connect abc 63077022')
+    st.write('contact','xyz','56556')
 elif z=='java':
-    st.write('connect xyz 44246')
+    st.write('contact','xyz','742475')
 elif z=='c++':
-    st.write('connect kds 646530')
-st.button(background-color:'g')
+    st.write('contact','nkf','54667')
